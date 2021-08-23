c93f279aa967bf1959409d48fa9f224f5647aa58
josharmi <josh.armitage@outlook.com>
2021-08-23 15:20:39 +0100

    Convert functions to using CloudFormation and add pipeline definition

--- graviton2test.py
+++
@@ -1,9 +0,0 @@
-import json
-import platform
-
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps(f'Hello from {platform.uname()}!')
-    }
